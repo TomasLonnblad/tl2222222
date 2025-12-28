77416a3b81e8c5c6ed6b4dbc38809d0753144ef5
Tomas <tomaslonnblad@gmail.com>
2025-12-28 12:58:03 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
     remotePatterns:[
       {
-
+        protocol:"https",
+        hostname:"images.pexels.com",import type { NextConfig } from "next";
       }
     ]
   }
